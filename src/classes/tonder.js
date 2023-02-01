42e7f04728e1ba5c3ad009c9f7c80ff53d6c3351
Carlos Fuentes <fuentesc91@gmail.com>
2023-02-01 09:53:20 -0600

    Add webpack and visual improvements

--- src/classes/tonder.js
+++ src/classes/tonder.js
@@ -2,28 +2,81 @@ import { AES } from "crypto-js";
 
 export class MySDK {
 
-    constructor() {
-        this.url = ""
-    }
-    openWindow(url, width = 500, height = 500, left = 0, top = 0, params) {
-        console.log(url)
-        if (params) {
-            const queryString = new URLSearchParams(params).toString();
-            const encrypted = AES.encrypt(queryString, 'url-params-encrypt').toString()
-            const encodedURL = encodeURIComponent(encrypted);
-            url = url + "?" + encodedURL;
-            console.log(url)
-        }
-        const newWindow = window.open(url, '_blank', `width=${width},height=${height},left=${left},top=${top}`);
-        const checkoutInterval = setInterval(() => {
-            if (newWindow.closed) {
-                console.log("The tab was closed!");
-                clearInterval(checkoutInterval);
-            }
-        }, 500);
-    }
+    constructor({ apiKey, type, backgroundColor, color }) {
+        this.url = "http://localhost:8081/#/"
+        this.apiKey = apiKey
+        this.type = type
+        this.backgroundColor = backgroundColor
+        this.color = color
+        this.params = ""
 
-    handleMessage(event) {
+        this.tonderButton = document.createElement('button');
+        this.tonderButton.innerHTML = "Proceder al pago";
+        this.stylishButton(this.tonderButton)
+        this.tonderButton.onclick = this.openCheckout
+        document.getElementById("tonder-checkout").appendChild(this.tonderButton);
+    }
+    stylishButton = (element) => {
+        element.style.backgroundColor = this.backgroundColor
+        element.style.color = this.color
+        element.style.display = 'flex'
+        element.style.justifyContent = 'center'
+        element.style.border = 'none'
+        element.style.padding = '1rem'
+        element.style.borderRadius = '10px'
+        element.style.fontSize = '1rem'
+        element.style.width = '100%'
+        element.style.boxShadow = '0 3px 6px 0 rgba(0,0,0,0.16)'
+    }
+    setPayment = (paymentData) => {
+        this.paymentData = paymentData
+        console.log(this.paymentData)
+    }
+    openTabListener = (tab, button) => {
+        const tabInterval = setInterval(() => {
+            if (tab.closed) {
+                clearInterval(tabInterval);
+                button.disabled = false
+                button.innerHTML = 'Proceder al pago'
+            }
+        }, 500)
+    }
+    openCheckout = () => {
+        const params = {...this.paymentData, apiKey: this.apiKey, type: this.type}
+        const queryString = new URLSearchParams(params).toString();
+        const encrypted = AES.encrypt(queryString, 'url-params-encrypt').toString()
+        const encodedURL = encodeURIComponent(encrypted);
+        this.params = "?" + encodedURL;
+        const newWindow = window.open(this.url + this.params, '_blank');
+        this.tonderButton.disabled = true
+        this.tonderButton.innerHTML = `
+            <div class="loader"></div>
+            <style>
+                .loader {
+                border: 4px solid ${this.color};
+                border-radius: 50%;
+                border-top: 4px solid ${this.backgroundColor};
+                width: 0.625rem;
+                height: 0.625rem;
+                -webkit-animation: spin 2s linear infinite; /* Safari */
+                animation: spin 2s linear infinite;
+                }
+
+                /* Safari */
+                @-webkit-keyframes spin {
+                0% { -webkit-transform: rotate(0deg); }
+                100% { -webkit-transform: rotate(360deg); }
+                }
+
+                @keyframes spin {
+                0% { transform: rotate(0deg); }
+                100% { transform: rotate(360deg); }
+                }
+            </style>
+        `
+        this.openTabListener(newWindow, this.tonderButton)
+    }
+    handleMessage = (event) => {
         if (event.origin === this.url) {
             console.log('Received message: ' + event.data);
         }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import { MySDK } from "./classes/tonder";
 
-const sdk = new MySDK();
+const config = {
+    apiKey:"a1f4db756764234940828bf0eb6c4d07588bf454",
+    type:"full",
+    backgroundColor: '#4ecfd6',
+    color: '#fff'
+}
+const sdk = new MySDK(config);
 const params = {
-    "apiKey":"a60caf40384ae8ff16cab82f70fc6fc0a0297b54",
-    "email":"123@gmail.com",
-    "type":"payment",
-    "product_name":"Playera",
-    "product_price":"400",
-    "product_image":"https://cdn.shopify.com/s/files/1/0532/1025/1461/products/IPLAYBANGblueberryraspberry.jpg?v=1657302782",
-    "shipping_cost":"150",
+    email:"123@gmail.com",
+    productName:"Playera",
+    productPrice:"400.12",
+    productImage:"https://cdn.shopify.com/s/files/1/0532/1025/1461/products/IPLAYBANGblueberryraspberry.jpg?v=1657302782",
+    shippingCost:"150",
 }
-const btn = document.createElement('button');
-btn.innerHTML = "Open Checkout";
-btn.onclick = function(){
-    sdk.openWindow('http://checkout.tonder.io/#/', 800, 600, 100, 100,params);
-}
-document.getElementById("tonder-checkout").appendChild(btn);
+sdk.setPayment(params)
