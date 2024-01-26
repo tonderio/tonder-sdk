95f223700a49e0fb84cfee2926e2d1cec0aaa980
Fábio Luna <fabiocluna@proton.me>
2024-01-26 20:43:50 -0300

    ✨ 3DS Iframe
    Added new methods to handle 3DS iframes and also to handle 3DS challenge
    for Azul.

--- src/classes/3dsHandler.js
+++ src/classes/3dsHandler.js
@@ -26,6 +26,22 @@ export class ThreeDSHandler {
     return localStorage.getItem("verify_transaction_status_url") 
   }
 
+  loadIframe() {
+    const iframe = this.payload?.next_action?.iframe_resources?.iframe
+
+    if (iframe) {
+      this.saveVerifyTransactionUrl()
+      const container = document.createElement('div')
+      container.innerHTML = iframe
+      document.body.appendChild(container)
+      return true
+    } else {
+      console.log('No redirection found');
+      return false
+    }
+
+  }
+
   redirectTo3DS() {
     const url = this.payload?.next_action?.redirect_to_url?.url
     if (url) {
@@ -51,6 +67,60 @@ export class ThreeDSHandler {
     }
 
     return parameters;
+  }
+
+  handleSuccessTransaction(response) {
+    this.removeVerifyTransactionUrl();
+    window.location = this.successUrl
+    console.log('Transacción autorizada exitosamente.');
+    return response;
+  }
+
+  handleDeclinedTransaction(response) {
+    this.removeVerifyTransactionUrl();
+    console.log('Transacción rechazada.');
+    throw new Error("Transacción rechazada.");
+  }
+
+  // TODO: the method below needs to be tested with a real 3DS challenge
+  // since we couldn't get a test card that works with this feature
+  async handle3dsChallenge(response_json) {
+    // Create the form element:
+    const form = document.createElement('form');
+    form.name = 'frm';
+    form.method = 'POST';
+    form.action = response_json.redirect_post_url;
+
+    // Add hidden fields:
+    const creqInput = document.createElement('input');
+    creqInput.type = 'hidden';
+    creqInput.name = response_json.creq;
+    creqInput.value = response_json.creq;
+    form.appendChild(creqInput);
+
+    const termUrlInput = document.createElement('input');
+    termUrlInput.type = 'hidden';
+    termUrlInput.name =  response_json.term_url;
+    termUrlInput.value = response_json.TermUrl;
+    form.appendChild(termUrlInput);
+
+    // Append the form to the body:
+    document.body.appendChild(form);
+    form.submit();
+
+    await this.verifyTransactionStatus();
+  }
+
+  async handleTransactionResponse(response) {
+    const response_json = await response.json();
+
+    if (response_json.status === "Pending") {
+      return await this.handle3dsChallenge(response_json);
+    } else if (["Success", "Authorized"].includes(response_json.status)) {
+      return this.handleSuccessTransaction(response);
+    } else {
+      return this.handleDeclinedTransaction(response);
+    }
   }
 
   async verifyTransactionStatus() {
